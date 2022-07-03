27b7caad7d3aa4865be3f4fa079624f3d7e3d14f
Carlos Hiroyuki Shimabukuro <carlos@digitalone.com.br>
2022-07-03 15:45:19 -0300

    Middlewares done. Challenge finished!

--- src/index.js
+++ src/index.js
@@ -11,18 +11,71 @@ const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find((user) => user.username === username);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
   // Complete aqui
+  const { user } = request;
+
+  const canCreateTodo = user.todos.length >= 10 && user.pro === false ? false : true;
+
+  if (!canCreateTodo) {
+    return response.status(403).json({ error: "Limit of todos reached. Please upgrade to pro plan!" })
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
   // Complete aqui
+  const { username } = request.headers;
+  const { id } = request.params;
+
+  const user = users.find((user) => user.username === username);
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" })
+  }
+
+  const validId = validate(id);
+  if (!validId) {
+    return response.status(400).json({ error: "Id not valid!" })
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: "Todo not exists!" });
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next()
+
 }
 
 function findUserById(request, response, next) {
   // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: "User not found!" });
+  }
+
+  request.user = user;
+  return next();
 }
 
 app.post('/users', (request, response) => {
